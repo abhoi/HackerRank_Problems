18f32930c52fd512c40cb205ce67f385a02a4bd7
amlaanb <amlaanb@gmail.com>
2016-11-02 21:25:47 +0530

    Added kangaroos

---
+++ kangaroos.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        
+        // compare x1 + y * v1 = x2 + y * v2 where y = no. of jumps
+        if (v1 > v2 && ((x2 - x1) % (v1 - v2) == 0))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
